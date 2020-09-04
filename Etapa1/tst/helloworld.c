6a21fef56322a69d6a196694cf49409e0f55ebfc
Artur <arturwaquil@gmail.com>
2020-09-04 17:40:50 -0300

    add test files

---
+++ Etapa1/tst/helloworld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* dummy c file */
+int main() {
+    /*
+        this is a multi
+                     line
+                        comment!
+        blablabla
+    */
+    printf("hello world!");
+    return 0;
+}
